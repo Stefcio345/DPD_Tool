d30e90db422e61ed3a7b8ea677ef02cde4a8bf74
Stefcio345 <kepskifranio@gmail.com>
2025-01-10 20:12:00 +0100

    Fixing docker compatibility

--- DPD_App/Program.cs
+++ DPD_App/Program.cs
@@ -11,6 +11,8 @@ SaveKeeper.LoadState();
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://0.0.0.0:5000");
+
 // Add mudBlazor services
 builder.Services.AddSingleton<AppState>();
 builder.Services.AddScoped<AppState>();
@@ -57,6 +59,8 @@ SaveKeeper.SaveState(SaveType.DEFAULT);
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+app.MapGet("/Test", () => "Hello, World!");
 
 app.Run();
 
