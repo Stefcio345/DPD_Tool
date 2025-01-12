daf08de837c35db6a4192f9177e85a728ed37528
Stefcio345 <kepskifranio@gmail.com>
2025-01-12 23:54:04 +0100

    Removed publish trimmed, Now works with docker

--- DPD_App/obj/Debug/net8.0/DPD_App.AssemblyInfo.cs
+++ DPD_App/obj/Debug/net8.0/DPD_App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DPD_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ebdc2b97b9564564e345459863cc8ea4a8b6172")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c16db4943e1b9fb81eb2cee22939cad4eb49f124")]
 [assembly: System.Reflection.AssemblyProductAttribute("DPD_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DPD_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
